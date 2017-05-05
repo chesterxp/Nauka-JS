4d71519305c99ec390516cbcc44e84422014dfa1
chesterxp <chesterxp7@wp.pl>
2017-05-05 10:42:35 +0200

    cwiczenie z zegarkiem analogowym

--- js/naukaJS.js
+++ js/naukaJS.js
@@ -116,9 +116,9 @@ function odliczanie() {
 
 	setTimeout("odliczanie()", 1000);
 }
-window.addEventListener('load',function(){
+//window.addEventListener('load',function(){
   odliczanie();
-})
+//})
 
 ///----------------FAQ---------------------
 
@@ -308,3 +308,25 @@ document.addEventListener('keyup',function(e){
   var letterBox = document.getElementById('key');
   letterBox.textContent = letter2;
 })
+
+
+
+//-------------analogClock------------------
+
+
+var secondsHand = document.querySelector('.seconds');
+var minutesHand = document.querySelector('.minutes');
+var hoursHand = document.querySelector('.hours');
+now = new Date();
+
+var setupClock = function(){
+  var secs = now.getSeconds();
+  var mins = now.getMinutes()*60;
+  var hours = now.getHours()*3600;
+  
+  secondsHand.style.animationDelay = '-' + secs + "s" ;
+  minutesHand.style.animationDelay = '-' + mins + 's';
+  hoursHand.style.animationDelay = '-' + hours + 's';
+}
+
+setupClock();
