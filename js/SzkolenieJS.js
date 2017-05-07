6f755d13600643d906a7c8fe85b5e9f4b9a9160b
chesterxp <chesterxp7@wp.pl>
2017-05-08 00:37:44 +0200

    rysujemy pączki, miernik z oceną produktu, poprawki na stronie, rozwijane menu

--- js/SzkolenieJS.js
+++ js/SzkolenieJS.js
@@ -112,7 +112,9 @@ function odliczanie() {
 	if (minuta < 10) minuta = "0" + minuta;
 	var sekunda = dzisiaj.getSeconds();
 	if (sekunda < 10) sekunda = "0" + sekunda;
-	document.getElementById('timer').innerHTML = dzien + " " + miesiac + " " + rok + "  - " + godzina + ":" + minuta + ":" + sekunda;
+    var timer = dzien + " " + miesiac + " " + rok + "  - " + godzina + ":" + minuta + ":" + sekunda;
+	document.getElementById('timer').innerHTML = timer;
+	document.getElementById('timer2').innerHTML = godzina + ":" + minuta + ":" + sekunda;
 
 	setTimeout("odliczanie()", 1000);
 }
@@ -122,9 +124,9 @@ function odliczanie() {
 
 ///----------------FAQ---------------------
 
-  $('.faq .answer').hide();
+  $('#faq .answer').hide();
   
-  $('.faq h2').click(function(){
+  $('#faq h2').click(function(){
     var $answer = $(this).next('.answer');
     if ($answer.is(':hidden')){
       $answer.slideDown();
@@ -329,3 +331,87 @@ var setupClock = function(){
 }
 
 setupClock();
+
+
+//-----------------console--------------------------
+//------console 1
+var wprowadz = document.getElementById('wprowadz');
+var zdanie = document.getElementById('zdanie');
+
+wprowadz.addEventListener('keyup',function(){
+  var letter = wprowadz.value;
+  zdanie.textContent = letter;
+});
+
+//------console 2
+var typing = document.querySelector('#typing2');
+var text = "";
+var startTyping = function (){
+  typing.textContent = "$";
+  text = wprowadz.value;
+  var currentIndex = 0;
+  if (text===""){
+    text = "Skoro nic nie wpisałeś ww. polu to sam  coś wpisze. Witam na mojej stronie z ćwiczeniami CSS oraz JS!!! Have fun!!!"
+  }
+  var interval = setInterval(function(){
+    if (text.length>currentIndex){
+    typing.textContent+=text[currentIndex];
+    currentIndex+=1;
+  } 
+    else{
+      clearInterval(interval);
+    }
+  },50 + Math.random()*50)
+};
+
+var writeSomething = document.getElementById('writeSomething');
+writeSomething.addEventListener('click',startTyping)
+
+///-------------howManyLetters-------------------------
+var input = document.querySelector('#customInput');
+var charCount = document.querySelector('#charCount');
+var onKeyDown = function(){
+  charCount.textContent = input.value.length;
+};
+input.addEventListener('keyup',onKeyDown);
+
+
+///-------------mouseMove-------------------------
+$('#hamburgerButtonBox').click(function(){
+  $('.burgerMenu').addClass('.showBurgerMenu');
+});
+
+///-------------mouseMove-------------------------
+
+var posX = document.getElementById('posX');
+var posY = document.getElementById('posY');
+var posPX = document.getElementById('posPX');
+var posPY = document.getElementById('posPY');
+
+
+document.addEventListener('mousemove',function(e){
+  var x = e.clientX;
+  var y = e.clientY;
+  
+  var px = e.pageX;
+  var py = e.pageY;
+  
+  posX.textContent = x;
+  posY.textContent = y;
+  
+  posPX.textContent = px;
+  posPY.textContent = py;
+});
+
+
+//------------------------------stars------------
+
+var countStars = function(){
+  var checkedRadio = document.querySelector('.stars input:checked');
+  var rate = document.getElementById('rate');
+  rate.textContent = checkedRadio.value;
+};
+
+var stars = document.getElementById('stars');
+
+stars.addEventListener('click',countStars);
